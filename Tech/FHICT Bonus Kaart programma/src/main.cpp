7aa517ad00b2552143fceed82ffccdf40bc16092
Bart Gool <bart.vangool@student.fontys.nl>
2022-04-13 15:11:43 +0200

    Replaced project again to swich framework + lcd integration

--- Tech/FHICT Bonus Kaart programma/src/main.cpp
+++ Tech/FHICT Bonus Kaart programma/src/main.cpp
@@ -1,12 +1,14 @@
+#include <LiquidCrystal.h>
 #include <SPI.h>
 #include <MFRC522.h>
  
 #define SS_PIN 10
 #define RST_PIN 9
-#define RED 2
-#define Green 3
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
- 
+
+const int rs = 8, en = 5, d4 = 6, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 void setup() 
 {
   Serial.begin(9600);   // Initiate a serial communication
@@ -14,24 +16,32 @@ void setup()
   mfrc522.PCD_Init();   // Initiate MFRC522
   Serial.println("Approximate your card to the reader...");
   Serial.println();
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
   pinMode(LED_BUILTIN, OUTPUT);
    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-   digitalWrite(Green, LOW);
-   digitalWrite(RED, LOW);
-
 }
 void loop() 
 {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
     return;
   }
+
   // Select one of the cards
   if ( ! mfrc522.PICC_ReadCardSerial()) 
   {
     return;
   }
+
   //Show UID on serial monitor
   Serial.print("UID tag :");
   String content= "";
@@ -45,23 +55,17 @@ void loop()
   Serial.println();
   Serial.print("Message : ");
   content.toUpperCase();
-  if ((content.substring(1) == "09 21 F0 B8") || (content.substring(1) == "CC CC A3 16") ) //change here the UID of the card/cards that you want to give access
+  if ((content.substring(1) == "1A 7C 78 30") || (content.substring(1) == "CC CC A3 16") ) //change here the UID of the card/cards that you want to give access
   {
     Serial.println("Authorized access");
     Serial.println();
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    digitalWrite(Green, HIGH);
     delay(10000);
-    digitalWrite(Green, LOW);
   }
  
  else   {
-  
     Serial.println(" Access denied");
      digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    digitalWrite(RED, HIGH);
     delay(10000);
-    digitalWrite(RED, LOW);
-  
   }
 }
