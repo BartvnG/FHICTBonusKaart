b7897173b7b692139db47101e3fef8c224c2d7c6
Bart Gool <bart.vangool@student.fontys.nl>
2022-04-19 15:14:58 +0200

    Finished serial interperter, can be improved

--- Tech/FHICT Bonus Kaart programma/src/main.cpp
+++ Tech/FHICT Bonus Kaart programma/src/main.cpp
@@ -16,7 +16,18 @@ bool isCardPresent = false;
 
 bool opTijd;
 unsigned long lastCardPresent;
-unsigned long lastInterfaceWipe;
+unsigned long lastDataShown;
+
+String serialMessage = "";
+bool communicatieGestart = false;
+bool checkInProtocol = false;
+const char  protocolStartChar = '#';
+const char  protocolEndChar  = '%';
+
+int dataIndex = 0;
+String naam = "";
+int streak = 0;
+int punten = 0;
 
 const int rs = 8, en = 5, d4 = 6, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
@@ -37,34 +48,96 @@ void setup()
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
 }
 
-void rgbLedWrite(int rVal, int gVal)
+void rgLedWrite(int rVal, int gVal)
 {
   analogWrite(rPin, rVal);
   analogWrite(gPin, gVal);
 }
 
 void UpdateInterface(String name, bool opTijd, int streak, int punten) {
-  lastInterfaceWipe = millis();
+  lastDataShown = millis();
   String streakString = String(streak);
   lcd.print(name + ": " + punten + " punten");
   lcd.setCursor(0,1);
   lcd.print("Streak: " + streakString);
   if (opTijd) {
-    rgbLedWrite(0, 150);
+    rgLedWrite(0, 150);
   }
   else {
-    rgbLedWrite(150, 0);
+    rgLedWrite(150, 0);
+  }
+}
+
+void InterpretSerialInput() {
+  if (Serial.available() > 0) {
+    char readChar = Serial.read();
+    if (readChar == protocolStartChar) {
+      serialMessage = "";
+      communicatieGestart = true;
+    }
+    else if (communicatieGestart) {
+      if (checkInProtocol) {
+        if (readChar == ' ') {
+          switch (dataIndex)
+          {
+            case 0:
+              naam = serialMessage;
+              break;
+            case 1:
+              streak = serialMessage.toInt();
+              break;
+            case 2:
+              punten = serialMessage.substring(1).toInt();
+              break;
+            default:
+              break;
+          }
+          dataIndex++;
+          serialMessage = "";
+        }
+      }
+
+      if (serialMessage.startsWith("CheckIn ")) {
+        checkInProtocol = true;
+        serialMessage = "";
+      }
+      
+      if (readChar == protocolEndChar) {
+        communicatieGestart = false;
+        dataIndex = 0;
+        serialMessage = "";
+        //Do stuff
+        if (checkInProtocol) {
+          UpdateInterface(naam, opTijd, streak, punten);
+          lastDataShown = millis();
+          Serial.println("\nNaam: " + naam);
+          Serial.print("op tijd?: ");
+          Serial.println(opTijd);
+          Serial.print("Streak: ");
+          Serial.println(streak);
+          Serial.print("punten: ");
+          Serial.println(punten);
+          checkInProtocol = false;
+        }
+      }
+      else {
+        serialMessage += readChar;
+      }
+    }
   }
 }
 
 void loop() 
 {
-  if (millis() - lastInterfaceWipe >= 2000) {
-    lcd.clear();
-    rgbLedWrite(0, 0);
-    lastUid = "replace text";
-  }
+  InterpretSerialInput();
+  //Wipe Interface 2 seconds after displaying data
+  // if (millis() - lastDataShown >= 2000) {
+  //   lcd.clear();
+  //   rgLedWrite(0, 0);
+  //   lastUid = "replace text";
+  // }
 
+  //Card reading logic
   if (millis() - lastCardPresent >= 500) {
     // Look for new cards
     if ( ! mfrc522.PICC_IsNewCardPresent()) 
@@ -83,12 +156,11 @@ void loop()
     for (byte i = 0; i < mfrc522.uid.size; i++) 
     {
       uid = "";
-      // Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-      // Serial.print(mfrc522.uid.uidByte[i], HEX);
+      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+      Serial.print(mfrc522.uid.uidByte[i], HEX);
       uid.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
       uid.concat(String(mfrc522.uid.uidByte[i], HEX));
     }
-
     if (uid != lastUid) {
       if (millis() / 1000 > 10) {
         opTijd = false;
@@ -102,7 +174,7 @@ void loop()
       //Send data to the C# program
       //Get data from the C# program
       //Read data to the interface
-      UpdateInterface(uid, opTijd, 4, 25);
+      //UpdateInterface(uid, opTijd, 4, 25);
       lastUid = uid;
     }
     lastCardPresent = millis();
